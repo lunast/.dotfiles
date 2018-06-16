74ef50df69fb9d35a4d64a8f5e8412405328f7f4
Tsubasa Toki <erwincat0812@gmail.com>
2018-06-16 17:05:29 +0900

    ディレクトリ名をdotfilesから.dotfilesに変更

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/Xmodmap ~/.Xmodmap
-ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
-ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/Xmodmap ~/.Xmodmap
+ln -sf ~/.dotfiles/latexmkrc ~/.latexmkrc
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
