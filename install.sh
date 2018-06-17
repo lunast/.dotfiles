ea8d96efae69091708e797585d18ca79b3c3e250
Tsubasa Toki <erwincat0812@gmail.com>
2018-06-17 15:41:28 +0900

    フォントを追加

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+[ -d ~/.fonts ] || mkdir ~/.fonts
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/Xmodmap ~/.Xmodmap
 ln -sf ~/.dotfiles/latexmkrc ~/.latexmkrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/fonts/UbuntuMonoNerdFontComplete-Bold.ttf ~/.fonts/UbuntuMonoNerdFontComplete-Bold.ttf
+ln -sf ~/.dotfiles/fonts/UbuntuMonoNerdFontComplete-Regular.ttf ~/.fonts/UbuntuMonoNerdFontComplete-Regular.ttf
