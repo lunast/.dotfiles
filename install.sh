f3d132519b2c4e8eb4af1f8cfed2d2b1c5d4514b
Tsubasa Toki <erwincat0812@gmail.com>
2018-06-19 15:44:17 +0900

    ctagsの設定ファイルを追加

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/Xmodmap ~/.Xmodmap
 ln -sf ~/.dotfiles/latexmkrc ~/.latexmkrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/ctags ~/.ctags
 ln -sf ~/.dotfiles/fonts/UbuntuMonoNerdFontComplete-Bold.ttf ~/.fonts/UbuntuMonoNerdFontComplete-Bold.ttf
 ln -sf ~/.dotfiles/fonts/UbuntuMonoNerdFontComplete-Regular.ttf ~/.fonts/UbuntuMonoNerdFontComplete-Regular.ttf
